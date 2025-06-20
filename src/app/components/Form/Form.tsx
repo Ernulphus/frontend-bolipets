152f29cccbdd09dc806ab14aadd9372f0e5434ce
Boaz Kaufman <bk1nyc@gmail.com>
2025-06-19 22:53:26 -0400

    Add text question component

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+function shortTextQuestion(
+  name='text',
+  label='text_question',
+) {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input type="text" name={name} />
+    </div>
+  )
+}
+
 export default function Form() {
   return (
     <form>
