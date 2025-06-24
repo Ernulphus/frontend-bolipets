0a350c86ce2c8535bd112d5ed1db03c8749a35f1
Boaz Kaufman <bk1nyc@gmail.com>
2025-06-24 17:13:45 -0400

    Comment out add pets form from pets page

--- src/app/Pets/page.tsx
+++ src/app/Pets/page.tsx
@@ -14,69 +14,69 @@ const PETS_CREATE_ENDPOINT = getURL(epGroups.PETS, methods.CREATE);
 type HTMLINPUTEVENT = React.ChangeEvent<HTMLInputElement>;
 type HTMLSELECTEVENT = React.ChangeEvent<HTMLSelectElement>
 
-function AddPetForm(props: AddPetFormProps) {
-  const {
-    visible,
-    cancel,
-    fetchPets,
-    setError,
-    roleOptions,
-  } = props;
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [role, setRole] = useState('');
+// function AddPetForm(props: AddPetFormProps) {
+//   const {
+//     visible,
+//     cancel,
+//     fetchPets,
+//     setError,
+//     roleOptions,
+//   } = props;
+//   const [name, setName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [role, setRole] = useState('');
 
-  const changeName = (event: HTMLINPUTEVENT) => { setName(event.target.value); };
-  const changeEmail = (event: HTMLINPUTEVENT) => { setEmail(event.target.value); };
-  const changeRole = (event: HTMLSELECTEVENT) => { setRole(event.target.value); };
+//   const changeName = (event: HTMLINPUTEVENT) => { setName(event.target.value); };
+//   const changeEmail = (event: HTMLINPUTEVENT) => { setEmail(event.target.value); };
+//   const changeRole = (event: HTMLSELECTEVENT) => { setRole(event.target.value); };
 
-  const addPet = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault();
-    const newPet = {
-      name: name,
-      email: email,
-      roles: role,
-      affiliation: '',
-    }
-    axios.put(PETS_CREATE_ENDPOINT, newPet)
-      .then(fetchPets)
-      .catch((error) => { setError(`There was a problem adding the pet. ${error}`); });
-  };
+//   const addPet = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+//     event.preventDefault();
+//     const newPet = {
+//       name: name,
+//       email: email,
+//       roles: role,
+//       affiliation: '',
+//     }
+//     axios.put(PETS_CREATE_ENDPOINT, newPet)
+//       .then(fetchPets)
+//       .catch((error) => { setError(`There was a problem adding the pet. ${error}`); });
+//   };
 
-  if (!visible) return null;
-  return (
-    <form>
-      <label htmlFor="name">
-        Name
-      </label>
-      <input required type="text" id="name" value={name} onChange={changeName} />
-      <label htmlFor="email">
-        Email
-      </label>
-      <input required type="text" id="email" onChange={changeEmail} />
-      <select required name='role' onChange={changeRole}>
-        {
-          Object.keys(roleOptions).map((code) => (
-            <option key={code} value={code}>
-              {roleOptions[code]}
-            </option>
-          ))
-        }
-      </select>
+//   if (!visible) return null;
+//   return (
+//     <form>
+//       <label htmlFor="name">
+//         Name
+//       </label>
+//       <input required type="text" id="name" value={name} onChange={changeName} />
+//       <label htmlFor="email">
+//         Email
+//       </label>
+//       <input required type="text" id="email" onChange={changeEmail} />
+//       <select required name='role' onChange={changeRole}>
+//         {
+//           Object.keys(roleOptions).map((code) => (
+//             <option key={code} value={code}>
+//               {roleOptions[code]}
+//             </option>
+//           ))
+//         }
+//       </select>
 
-      <button type="button" onClick={cancel}>Cancel</button>
-      <button type="submit" onClick={addPet}>Submit</button>
-    </form>
-  );
-}
-interface AddPetFormProps {
-  visible: boolean,
-  cancel: () => void,
-  fetchPets: () => void,
-  // setError: (arg0?: string) => void,
-  setError: React.Dispatch<React.SetStateAction<string>>,
-  roleOptions: { [key: string]: string },
-}
+//       <button type="button" onClick={cancel}>Cancel</button>
+//       <button type="submit" onClick={addPet}>Submit</button>
+//     </form>
+//   );
+// }
+// interface AddPetFormProps {
+//   visible: boolean,
+//   cancel: () => void,
+//   fetchPets: () => void,
+//   // setError: (arg0?: string) => void,
+//   setError: React.Dispatch<React.SetStateAction<string>>,
+//   roleOptions: { [key: string]: string },
+// }
 
 function ErrorMessage(props: ErrorMessageProps) {
   const { message } = props;
