425508c59f8ed6351f6c53af0e80542107404baa
Boaz Kaufman <bk1nyc@gmail.com>
2025-09-03 23:13:30 -0400

    Start dedicated pet preview component

---
+++ src/app/PetPreview/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Form.module.css';
+import { StaticImageData } from 'next/image';
+
+interface PetPreviewProps {
+  color: string,
+  image: StaticImageData,
+}
+
+export default function PetPreview({color, image}: PetPreviewProps) {
+  return (
+    <div className={styles.pet_preview}>
+      <div style={{backgroundColor: color}}/>
+      <img
+        src={image.src}
+      />
+    </div>
+  )
+}
