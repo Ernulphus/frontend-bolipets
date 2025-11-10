876c47e1c3f24602aebffdeb8ea1539b59ab2005
Boaz Kaufman <bk1nyc@gmail.com>
2025-11-10 00:04:03 -0500

    Set up auth0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { auth0 } from "../lib/auth0"; // Adjust path if your auth0 client is elsewhere
+
+export default async function Home() {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    );
+  }
+
   return (
-    <div>
-      <h1>Bolipets!</h1>
-    </div>
-  )
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+    </main>
+  );
 }
